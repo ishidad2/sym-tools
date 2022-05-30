638974b8fe09c88964217d062c7f6c1a259006fe
ishidad2 <d.m.y.s.happy.life@gmail.com>
2022-05-30 10:00:18 +0900

    init

---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <router-link to="/demo1">demo1</router-link> |
+    <router-link to="/demo2">demo2</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      links: [
+
+      ]
+    }
+  }
+}
+</script>
